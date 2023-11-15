b422149787c360237f066345adb903dceb3d6356
VitorHugoAntunes <vitor4ntunes@gmail.com>
2023-11-15 20:33:12 -0300

    Refresh token, connection with Google API, create schedule route, Schedule form and form confirmation

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -15,6 +15,10 @@ export function buildNextAuthOptions(
         clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
         authorization: {
           params: {
+            // Pedir para o usuario se logar sempre que entrar de novo na aplicacao para resetar o refresh token
+            prompt: 'consent',
+            access_type: 'offline',
+            response_type: 'code',
             // Scope significa qual aplicacao ou informacao queremos ter permissao do usuario
             scope:
               'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
